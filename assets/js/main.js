eb6bc481a9f59c01faa34937edb2145fcbeb63f6
barsolga <s219165@student.uek.krakow.pl>
2023-04-06 20:23:43 +0200

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,40 +13,4 @@ if ("serviceWorker" in navigator) {
 		});
 }
 
-const footerTime = document.querySelector("#footer-time");
-const map = document.querySelector("#frame");
-footerTime.textContent = new Date().getFullYear();
-
-if (map) {
-	let url = "";
-
-	const options = {
-		enableHighAccuracy: true,
-		timeout: 5000,
-		maximumAge: 0,
-	};
-
-	function success(pos) {
-		const crd = pos.coords;
-		const lat = crd.latitude;
-		const lon = crd.longitude;
-
-		console.log("Your current position is:");
-		console.log(`Latitude : ${crd.latitude}`);
-		console.log(`Longitude: ${crd.longitude}`);
-		console.log(`More or less ${crd.accuracy} meters.`);
-
-		url = `https://www.openstreetmap.org/export/embed.html?bbox=${lon}%2C${lat}&amp;layer=mapnik`;
-		map.setAttribute("src", url);
-	}
-
-	function error(err) {
-		console.warn(`ERROR(${err.code}): ${err.message}`);
-	}
-
-	navigator.geolocation.getCurrentPosition(success, error, options);
-}
-
-if (sosBtn) {
-	
-}
+document.querySelector("#footer-time").textContent = new Date().getFullYear();
